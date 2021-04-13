6b6130a7a1c71c37240937b0275e3a21e787933a
Andrés Abraham Hernández Sierra <abraham.hs@gmail.com>
2021-04-13 19:27:23 +0000

    Correccion datetime modulos 2

--- TiendaCooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
+++ TiendaCooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -11,5 +11,5 @@ class Tarea(models.Model):
     tarea = fields.Char(string='Tarea')
     descripcion = fields.Char(string='Descripcion')
     tipoTarea = fields.Char(string='TipoTarea')
-    horaInicio = fields.Datetime.to_datetime(string ="HoraInicio", default=datetime.now())
-    horaFin = fields.Datetime.to_datetime(string ="HoraFin", default=datetime.now())
+    horaInicio = fields.Datetime.to_datetime(string ="HoraInicio", default=fields.Datetime.now())
+    horaFin = fields.Datetime.to_datetime(string ="HoraFin", default=fields.Datetime.now())
