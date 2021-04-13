b89a312fbf26ae4365671c46741dab2c72da7c11
Andrés Abraham Hernández Sierra <abraham.hs@gmail.com>
2021-04-13 19:00:18 +0000

    Correccion de modulos datetime

--- TiendaCooperativa/models/tarea.py
+++ TiendaCooperativa/models/tarea.py
@@ -10,5 +10,5 @@ class Tarea(models.Model):
     tarea = fields.Char(string='Tarea')
     descripcion = fields.Char(string='Descripcion')
     tipoTarea = fields.Char(string='TipoTarea')
-    horaInicio = fields.DateTime(string ="HoraInicio", default=datetime.now())
-    horaFin = fields.DateTime(string ="HoraFin", default=datetime.now())
+    horaInicio = fields.Datetime.to_datetime(string ="HoraInicio", default=datetime.now())
+    horaFin = fields.to_datetime(string ="HoraFin", default=datetime.now())
