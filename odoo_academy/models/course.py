9d636c6e14e46b6feec5a9226d19ccab39399587
aabrahamhs <abraham.hs@gmail.com>
2021-04-16 00:53:57 +0000

    ORM avanzado

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError,ValidationError
 
 class Course(models.Model):
     
@@ -17,3 +18,19 @@ class Course(models.Model):
                             copy=False)
     active = fields.Boolean(string='Active', default=True)
     
+    precio_base=fields.Float(string='Precio base', default=0.00)
+    tarifa_adicional=fields.Float(string='Tarifa adicional', default=10.00)
+    precio_total=fields.Float(string='Precio total', readonly=True)
+    
+    @api.onchange('precio_base','tarifa_adicional')
+    def _onchange_precio_total(self):
+        if self.precio_base < 0.0:
+            raise UserError("precio base no puede ser negativo")
+        self.precio_total=self.precio_base+self.tarifa_adicional
+        
+    @api.constrains('tarifa_adicional')
+    def _checa_tarifa_adicional(self):
+        for record in self:
+            if record in self:
+                if record.tarifa_adicional < 10.00:
+                    raise ValidationError('Tarifa adicional no puede ser menor que %s' % record.tarifa_adicional )
