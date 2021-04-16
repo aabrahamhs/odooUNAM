731d6685ec39640965bf0acbd5d172551d6b350a
aabrahamhs <abraham.hs@gmail.com>
2021-04-16 15:51:37 +0000

    ORM avanzado tienda

--- TiendaCooperativa/models/tarea.py
+++ TiendaCooperativa/models/tarea.py
@@ -10,5 +10,17 @@ class Tarea(models.Model):
     tarea = fields.Char(string='Tarea')
     descripcion = fields.Char(string='Descripcion')
     tipoTarea = fields.Char(string='TipoTarea')
+    lider=fields.Char(string='Lider')
+    estado = fields.Selection(string='Estado',
+                            selection=[('borrador','Borrador'),
+                                       ('listo','Listo'),
+                                       ('enprogreso','En progreso'),
+                                      ('terminado','Terminado')],
+                            copy=False)
     horaInicio = datetime.strftime(fields.Datetime.now(), "%Y-%m-%d %H:%M:%S")
     horaFin = datetime.strftime(fields.Datetime.now(), "%Y-%m-%d %H:%M:%S")
+    
+    @api.onchange('lider')
+    def _pasar_estado_a_listo(self):
+        if len(self.lider) > 0 :
+            estado = 'listo'
