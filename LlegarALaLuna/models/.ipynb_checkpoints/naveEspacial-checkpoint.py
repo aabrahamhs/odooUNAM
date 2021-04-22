687c864b165b5d572cc2f3e8b9e3ccfabb734bd2
aabrahamhs <abraham.hs@gmail.com>
2021-04-22 01:27:03 +0000

    Nave graficos

--- LlegarALaLuna/models/.ipynb_checkpoints/naveEspacial-checkpoint.py
+++ LlegarALaLuna/models/.ipynb_checkpoints/naveEspacial-checkpoint.py
@@ -10,6 +10,7 @@ class NaveEspacial(models.Model):
     
     mision_id = fields.Many2one(comodel_name='academy.mision',string='Mision',ondelete='cascade')
     
+    nombre=fields.Char(string='Nombre')
     alto = fields.Integer(string='Alto')
     ancho = fields.Integer(string='Ancho')
     largo = fields.Integer(string='Largo')
@@ -18,13 +19,17 @@ class NaveEspacial(models.Model):
     tipoBarco = fields.Char(string='TipoBarco')
     nopasajeros = fields.Integer(string='Nopasajeros')
     
+    cantidad_combustible=fields.Integer(string='cantidad_combustible')
+    no_motores=fields.Integer(string='no_motores')
+    no_caracteristicas_seguridad=fields.Integer(string='no_caracteristicas_seguridad')
+    
     active = fields.Boolean(string='Active', default=True)
     
     @api.constrains('ancho','largo')
     def _valida_tamanio(self):
         for record in self:
             if record.largo <= record.ancho:
-                raise UserError("El ancho no puede ser menor que el largo")
+                raise UserError("El ancho no puede ser mayor que el largo")
     
     
     
