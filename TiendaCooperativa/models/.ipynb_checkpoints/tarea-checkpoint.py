8f002fc222a30438f095dcee0eb1900cf29989ac
aabrahamhs <abraham.hs@gmail.com>
2021-04-22 00:20:04 +0000

    Tienda coperativa Voluntarios

--- TiendaCooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
+++ TiendaCooperativa/models/.ipynb_checkpoints/tarea-checkpoint.py
@@ -20,6 +20,8 @@ class Tarea(models.Model):
     horaInicio = datetime.strftime(fields.Datetime.now(), "%Y-%m-%d %H:%M:%S")
     horaFin = datetime.strftime(fields.Datetime.now(), "%Y-%m-%d %H:%M:%S")
     
+    voluntarios_id=fields.Many2one(comodel_name='res.partner',string='Voluntarios')
+    
     @api.onchange('lider')
     def _pasar_estado_a_listo(self):
         print ("Lider_len=",self.lider)
